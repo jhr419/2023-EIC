b666cd4cc18cd83bde6ae5c7afe9f16733051ae7
BrandonHu <616267548@qq.com>
2023-10-25 01:01:51 +0800

    写好6020

--- Logistic_Robot_v1/MDK-ARM/Logistic_Robot_v1/queue.d
+++ Logistic_Robot_v1/MDK-ARM/Logistic_Robot_v1/queue.d
@@ -1,9 +1,9 @@
 logistic_robot_v1\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/queue.c
-logistic_robot_v1\queue.o: D:\engineering software\keil5\keil5 5.24\ARM\ARMCC\Bin\..\include\stdlib.h
-logistic_robot_v1\queue.o: D:\engineering software\keil5\keil5 5.24\ARM\ARMCC\Bin\..\include\string.h
+logistic_robot_v1\queue.o: D:\Develop\Keil V5\ARM\ARMCC\Bin\..\include\stdlib.h
+logistic_robot_v1\queue.o: D:\Develop\Keil V5\ARM\ARMCC\Bin\..\include\string.h
 logistic_robot_v1\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
-logistic_robot_v1\queue.o: D:\engineering software\keil5\keil5 5.24\ARM\ARMCC\Bin\..\include\stddef.h
-logistic_robot_v1\queue.o: D:\engineering software\keil5\keil5 5.24\ARM\ARMCC\Bin\..\include\stdint.h
+logistic_robot_v1\queue.o: D:\Develop\Keil V5\ARM\ARMCC\Bin\..\include\stddef.h
+logistic_robot_v1\queue.o: D:\Develop\Keil V5\ARM\ARMCC\Bin\..\include\stdint.h
 logistic_robot_v1\queue.o: ../Core/Inc/FreeRTOSConfig.h
 logistic_robot_v1\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
 logistic_robot_v1\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
--- Logistic_Robot_v1/application/motor_ctrl.c
+++ Logistic_Robot_v1/application/motor_ctrl.c
@@ -5,29 +5,7 @@
 #include "CAN_cmd_all.h"
 #include "commu_task.h"
 
-#define INIT_ECD 6000
-
-#define LimitMax(input, max)   \
-    {                          \
-        if (input > max)       \
-        {                      \
-            input = max;       \
-        }                      \
-        else if (input < -max) \
-        {                      \
-            input = -max;      \
-        }                      \
-    }
-
-//typedef struct 
-//{
-//	uint16_t ecd_init;
-//	const motor_6020_measure_t*	motor_6020;
-//	uint16_t* ecd_p;
-//  int16_t* speed_rpm_p;
-//}m6020_t;
-
-//m6020_t* motor_6020;
+#define INIT_ECD 0
 		
 const motor_6020_measure_t*	motor_6020;
 pid_t pid_angle_6020;
@@ -39,13 +17,9 @@ uint8_t cnt = 0;
 
 uint16_t ecd_format(uint16_t ecd)
 {
-	if(ecd >= 8192)
+	while (ecd >= 8192)
 	{
-		return ecd - 8192;
-	}
-	else if(ecd<0)
-	{
-		return ecd + 8192;
+		ecd -= 8192;
 	}
 	return ecd;
 }
@@ -56,18 +30,22 @@ uint16_t ecd_limit(uint16_t ref, uint16_t set)
 	{
 		if(set - ref > ref - set + MAX_MOTOR_ECD)
 		{
-			return set - MAX_MOTOR_ECD;
+			return set - MAX_MOTOR_ECD;//error = set - MAX_MOTOR_ECD - ref
 		}
 		else if(set - ref < ref - set + MAX_MOTOR_ECD)
 		{
-			return set;
+			return set;//error = set -ref
 		}
 	}
 	else if(set < ref)
 	{
 		if(ref - set > set - ref + MAX_MOTOR_ECD)
 		{
-			return set + MAX_MOTOR_ECD;
+			return set + MAX_MOTOR_ECD;//error = 
+		}
+		else if(ref - set < set - ref + MAX_MOTOR_ECD)
+		{
+			return set;//error
 		}
 	}
 }
@@ -78,10 +56,6 @@ void angle_m6020_init(void)
 	PID_init(&pid_speed_6020, PID_POSITION, pid_6020_speed, 5000, 100);
 
 	motor_6020 = get_motor_6020_measure_point(0);	
-	//motor_6020->ecd_init=INIT_ECD;
-	//motor_6020->ecd_p = &(motor_6020->motor_6020->ecd);
-	//motor_6020->speed_rpm = motor_6020->motor_6020->speed_rpm;
-	
 	ecd[0] = INIT_ECD;
 	ecd[1] = ecd_format(ecd[0] + 2730);
 	ecd[2] = ecd_format(ecd[1] + 2731);
@@ -94,9 +68,16 @@ void angle_m6020_to_next(void)
 		cnt = 0;
 
 }
+
+
+fp32 PID_6020_calc(pid_t *pid, fp32 ref, fp32 set)
+{
+	PID_calc(pid, ref, ecd_limit(ref, set));
+}
+
 void give_pid_current_6020()
 {
-	PID_calc(&pid_angle_6020, motor_6020->ecd, ecd_limit(motor_6020->ecd, ecd[cnt]));
+	PID_6020_calc(&pid_angle_6020, motor_6020->ecd, ecd[cnt]);
 	PID_calc(&pid_speed_6020, motor_6020->speed_rpm, pid_angle_6020.out);
 	CAN_cmd_6020(pid_speed_6020.out,pid_speed_6020.out,pid_speed_6020.out,pid_speed_6020.out);
 	uart8_printf("%d,%d\r\n", ecd[cnt], motor_6020->ecd);
